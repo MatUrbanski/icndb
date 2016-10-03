aab8256dcef68a21c02c6f2990fc10c4c877afd1
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2016-10-03 21:47:35 +0200

    Add main_module for escript

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Icndb.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     escript: [main_module: Icndb.CLI],
      deps: deps()]
   end
 
